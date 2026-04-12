87392ced6e265c2c877076766bdfad610540ab0f
malkapurwalat-svg <malkapurwalat@gmail.com>
2026-04-12 07:45:19 +0100

    Update deploy-commands.js

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,25 +1,30 @@
+require("dotenv").config();
 const { REST, Routes } = require("discord.js");
-const { clientId, guildId, token } = require("./config");
+const fs = require("fs");
+const path = require("path");
 
-const commands = [
-  require("./commands/ga-help").data.toJSON(),
-  require("./commands/ga-create").data.toJSON(),
-  require("./commands/ga-view").data.toJSON(),
-  require("./commands/ga-list").data.toJSON(),
-  require("./commands/ga-start").data.toJSON(),
-  require("./commands/ga-repeat").data.toJSON(),
-  require("./commands/ga-end").data.toJSON(),
-  require("./commands/ga-reroll").data.toJSON()
-];
+const commands = [];
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
 
-const rest = new REST({ version: "10" }).setToken(token);
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  if (command.data) {
+    commands.push(command.data.toJSON());
+  }
+}
+
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
   try {
     console.log("🔄 Registering slash commands...");
 
     await rest.put(
-      Routes.applicationGuildCommands(clientId, guildId),
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
       { body: commands }
     );
 

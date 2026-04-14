b193606b33e72d6947002430f2a6cdd2f26f3996
malkapurwalat-svg <malkapurwalat@gmail.com>
2026-04-14 08:54:35 +0400

    Update ga-help.js

--- src/commands/ga-help.js
+++ src/commands/ga-help.js
@@ -17,11 +17,15 @@ module.exports = {
       .addFields(
         {
           name: "/gxcreate",
-          value: "Create and save a giveaway template with a unique token."
+          value: "Create and save a giveaway template."
+        },
+        {
+          name: "/gxedit",
+          value: "Edit a saved giveaway template using the same token."
         },
         {
           name: "/gxview",
-          value: "View the full saved details of one giveaway token."
+          value: "View the details of a saved giveaway token."
         },
         {
           name: "/gxlist",
@@ -29,15 +33,15 @@ module.exports = {
         },
         {
           name: "/gxstart",
-          value: "Start a giveaway from a saved token."
+          value: "Start a giveaway on your own posted message using token + message ID."
         },
         {
           name: "/gxrepeat",
-          value: "Repeat a saved giveaway using the same token settings."
+          value: "Repeat a saved giveaway on your own posted message."
         },
         {
           name: "/gxdelete",
-          value: "Delete a saved giveaway template and free that token again."
+          value: "Delete a saved giveaway template and free that token."
         },
         {
           name: "/gxend",
@@ -45,14 +49,18 @@ module.exports = {
         },
         {
           name: "/gxeroll",
-          value: "Pick a new winner for a completed giveaway."
+          value: "Reroll a completed giveaway."
+        },
+        {
+          name: "/gxpick",
+          value: "Manually choose a winner from the people who reacted."
         },
         {
           name: "/gxhelp",
           value: "Show this help menu."
         }
       )
-      .setFooter({ text: "Only you can see this help menu." });
+      .setFooter({ text: "Template placeholders supported in start/repeat: {time} {prize} {winners} {token} {winner_mentions}" });
 
     await interaction.reply({
       embeds: [embed],
